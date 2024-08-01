c09b84d1c286d65d62cdbce099e97176184376fc
Bao <yuan.truyenbao@gmail.com>
2024-08-01 15:05:39 -0600

    urdf and meshes for Britton

--- dvrk_env/shape_servo_control/src/data_collection/physical_dvrk/run_shinghei.sh
+++ dvrk_env/shape_servo_control/src/data_collection/physical_dvrk/run_shinghei.sh
@@ -15,3 +15,6 @@ cd /home/baothach/deformernet_core/learn_manipulation_points
 python3 process_data_dense_predictor_single.py --obj_category box_1kPa
 python3 process_data_dense_predictor_single.py --obj_category cylinder_1kPa
 python3 single_dense_predictor_trainer.py
+
+
+rosrun shape_servo_control collect_data_bimanual_physical_dvrk.py --deformernet_data_path /home/baothach/Documents/shinghei_def_data_temp --manipulation_point_data_path /home/baothach/Documents/shinghe_mani_data_temp --save_data True
